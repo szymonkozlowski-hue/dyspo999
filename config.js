dcea1a9f278935c7d8fe20a1e6b8fdac7cbf7ff7
szymonkozlowski-hue <szymon.kozlowski@gmail.com>
2026-09-06 19:34:56 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const CONFIG = {
   STATION_PASSWORD: "pgrm2026",
 
   // Twój darmowy klucz API z Google AI Studio:
-  GEMINI_API_KEY: "AIzaSyC-TvFlXWo9XAxed0bCW3HbJ8VpmPx-viI",
+  GEMINI_API_KEY: "QVEuQWI4Uk42SWtQeXRUQzlPb0FNMTJtWGlJVUNzdktlWkdtRGZZZG9hQXdfWDV2Q05saGc=",
 
   // Adres Twojego działającego serwera AED na Renderze:
   AED_API_URL: "https://aed-finder.onrender.com/find-aed"
